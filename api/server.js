088662d9b9cfd22ef587891277a7da4b93e707c0
cyrus <cyrus7@hotmail.fr>
2021-04-12 01:45:30 -0400

    make it better

--- api/server.js
+++ api/server.js
@@ -29,7 +29,7 @@ app.post("/api/hashtag", (req, res) => {
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
